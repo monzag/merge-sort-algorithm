551de999a9870f12466c37ba5dd4bed43fb5bdef
Matyas Forian Szabo <forianszm@gmail.com>
2017-07-10 11:19:14 +0200

    Initial code is done

---
+++ README.md
@@ -0,0 +1,4 @@
+# Merge Sort
+
+Repo to practive merge sort. Please do not copypaste code or just try to get the tests work, the point of this assignment is that you know this algorithm well.   
+Try to get all tests to pass!
---
+++ src/main/java/com/codecool/mergesort/MergeSort.java
@@ -0,0 +1,14 @@
+package com.codecool.mergesort;
+
+import java.util.List;
+
+public class MergeSort {
+
+    /**
+     * Sorts the given List in place
+     * @param toSort the List to sort. Throws an error if its null
+     */
+    public void sort(List<Integer> toSort) {
+
+    }
+}
