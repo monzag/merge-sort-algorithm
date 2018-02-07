9a23d858f0ea230301986c899e0d7957ee6d78a3
monzag <zaganiacz.m@gmail.com>
2018-02-07 20:54:22 +0100

    Add sort method

--- src/main/java/com/codecool/mergesort/MergeSort.java
+++ src/main/java/com/codecool/mergesort/MergeSort.java
@@ -8,7 +8,11 @@ public class MergeSort {
      * Sorts the given List in place
      * @param toSort the List to sort. Throws an error if its null
      */
-    public void sort(List<Integer> toSort) {
+    public void sort(List<Integer> toSort) throws IllegalArgumentException {
+        if (toSort == null) {
+            throw new IllegalArgumentException();
+        }
 
+        mergeSort(toSort);
     }
 }
