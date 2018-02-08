e6540564ce25ff1c232429cc6f8311823cbf23a3
monzag <zaganiacz.m@gmail.com>
2018-02-08 10:29:31 +0100

    Add fields numbers and temp

--- src/main/java/com/codecool/mergesort/MergeSort.java
+++ src/main/java/com/codecool/mergesort/MergeSort.java
@@ -4,15 +4,8 @@ import java.util.List;
 
 public class MergeSort {
 
-    /**
-     * Sorts the given List in place
-     * @param toSort the List to sort. Throws an error if its null
-     */
-    public void sort(List<Integer> toSort) throws IllegalArgumentException {
-        if (toSort == null) {
-            throw new IllegalArgumentException();
-        }
+    private List<Integer> numbers;
+    private Integer[] temp;
 
-        mergeSort(toSort);
-    }
+
 }
